ae1eb1960973c80534189dc58e950494d52adf6e
TanmayNawlakhe <tanawlakhe@gmail.com>
2025-02-09 10:11:24 +0530

    Second Commit

--- src/components/Howitworks.jsx
+++ src/components/Howitworks.jsx
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react'
 import { chipImg, frameImg, frameVideo } from '../utils'
 import { useGSAP } from '@gsap/react'
-import gsap from 'gsap'
+import gsap from'gsap'
 import { animateWithGsap } from '../utils/animations'
 
 const Howitworks = () => {
